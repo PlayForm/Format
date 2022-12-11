00e14df7c3e73bfb7c3ba536d8fa5a37d8639102
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-11 07:47:45 +0200

    squash!

--- dist/options/index.d.ts
+++ dist/options/index.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="node" resolution-mode="require"/>
-/// <reference types="node" resolution-mode="require"/>
 import type { Options as OptionsBase, optionCallbacksFile, optionCallbacksPipe } from "@nikolarhristov/pipeline/dist/options/index.js";
 import type ROME from "./rome.js";
 export type filterFn = (file: string) => boolean;
@@ -11,7 +9,7 @@ export declare const options: {
     files: string;
     pipeline: {
         wrote: (_file: string, data: string) => Promise<string>;
-        read: (file: import("fs").PathLike | import("fs/promises").FileHandle) => Promise<string>;
+        read: (file: string) => Promise<string>;
         passed: () => Promise<true>;
         failed: (inputPath: optionCallbacksFile["inputPath"]) => Promise<string>;
         accomplished: (inputPath: optionCallbacksFile["inputPath"], outputPath: optionCallbacksFile["outputPath"], _fileSizeBefore: optionCallbacksFile["fileSizeBefore"], _fileSizeAfter: optionCallbacksFile["fileSizeAfter"]) => Promise<string>;
