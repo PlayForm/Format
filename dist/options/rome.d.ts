9e69b4b8752a27170ae4b3312bf29a1b2e212bf8
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-14 00:42:11 +0200

    squash!

--- dist/options/rome.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Configuration } from "@rometools/wasm-nodejs";
-export interface ROME extends Configuration {
-    [key: string]: any;
-}
-declare const _default: {};
-export default _default;
