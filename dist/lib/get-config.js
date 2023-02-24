90959f29cc88f5b6baddfc41a9c56fce24c450b9
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-24 04:04:58 +0200

    Prettifies everything

--- dist/lib/get-config.js
+++ dist/lib/get-config.js
@@ -1,1 +1,6 @@
-import*as r from"fs";import{dirname as n,resolve as e}from"path";import{cwd as a}from"process";import{fileURLToPath as c}from"url";var p=async t=>{const o=e(`${a()}/${t}`);let i=r.readFileSync(e(`${n(c(import.meta.url))}/../config/${t}`),"utf-8").toString();try{await r.promises.access(o,r.constants.R_OK),i=r.readFileSync(o,"utf-8").toString()}catch{}return i};export{p as default};
+import * as r from "fs";
+import { resolve as e, dirname as n } from "path";
+import { cwd as a } from "process";
+import { fileURLToPath as c } from "url";
+var p=async t=>{const o=e(`${a()}/${t}`);let i=r.readFileSync(e(`${n(c(import.meta.url))}/../config/${t}`),"utf-8").toString();try{await r.promises.access(o,r.constants.R_OK),i=r.readFileSync(o,"utf-8").toString()}catch{}return i};export { p as default };
+
