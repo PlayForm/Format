22ad7ec9d3e8863d4f3265a99a5d5b5cdde73682
Nikola Hristov <nikola@nikolahristov.tech>
2023-03-09 18:36:20 +0200

    squash!

--- dist/options/rome.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Configuration } from "@rometools/wasm-nodejs";
-export interface ROME extends Omit<Configuration, "$schema"> {
-    [key: string]: unknown;
-}
-declare const _default: {};
-export default _default;
