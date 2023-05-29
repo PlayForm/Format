00784fcbd5ceacc9480a4c170d71b446c6273fa0
Nikola Hristov <nikola@nikolahristov.tech>
2023-05-30 01:58:48 +0300

    squash!

--- dist/options/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Options as OptionsBase } from "files-pipe/dist/options/index.js";
-import type { ROME } from "./rome.js";
-export interface Options extends OptionsBase {
-    [key: string]: any;
-    rome?: boolean | ROME;
-}
-declare const _default: Options;
-export default _default;
