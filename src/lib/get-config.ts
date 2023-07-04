093c3ffa4b18d870a65cb14d506295e51e421a78
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-04 23:56:20 +0300

    1.1.20

--- src/lib/get-config.ts
+++ src/lib/get-config.ts
@@ -5,21 +5,20 @@ import { cwd } from "process";
 import { fileURLToPath } from "url";
 
 export default async (file: string) => {
-	const fileWorking = resolve(`${cwd()}/${file}`);
-
-	let config = (
-		await readFile(
-			resolve(
-				`${dirname(fileURLToPath(import.meta.url))}/../config/${file}`
-			),
-			"utf-8"
-		)
-	).toString();
-
 	try {
-		await access(fileWorking, constants.R_OK);
-		config = (await readFile(fileWorking, "utf-8")).toString();
-	} catch (_error) {}
-
-	return config;
+		const working = resolve(`${cwd()}/${file}`);
+		await access(working, constants.R_OK);
+		return (await readFile(working, "utf-8")).toString();
+	} catch (_error) {
+		return (
+			await readFile(
+				resolve(
+					`${dirname(
+						fileURLToPath(import.meta.url)
+					)}/../config/${file}`
+				),
+				"utf-8"
+			)
+		).toString();
+	}
 };
