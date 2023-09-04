65430a585660c0f001f3beed72846d8f30b650ec
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-04 19:04:04 +0300

    squash!

--- Target/Option/Index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Option as _Option } from "files-pipe";
-import type { Rome } from "./Rome.js";
-export interface Option extends _Option {
-    [key: string]: any;
-    Rome?: boolean | Rome;
-}
-declare const _default: Option;
-export default _default;
--- Target/Option/Rome.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Configuration } from "@rometools/wasm-nodejs";
-export interface Rome extends Omit<Configuration, "$schema"> {
-    [key: string]: any;
-}
-declare const _default: {};
-export default _default;
