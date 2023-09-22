2129660d4850ede4e683183f338a5538581adca4
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-22 05:37:11 +0300

    squash!

--- Configuration/ESBuild.js
+++ Configuration/ESBuild.js
@@ -4,34 +4,33 @@ const esbuild_plugin_copy_1 = require("esbuild-plugin-copy");
 const promises_1 = require("fs/promises");
 const Out = "Target";
 exports.default = {
-    format: "esm",
-    minify: true,
-    outdir: Out,
-    platform: "node",
-    target: "esnext",
-    write: true,
-    plugins: [
-        {
-            name: "Target",
-            setup(Build) {
-                Build.onStart(async () => {
-                    try {
-                        await (0, promises_1.rm)(Out, {
-                            recursive: true,
-                        });
-                    }
-                    catch (_Error) { }
-                });
-            },
-        },
-        (0, esbuild_plugin_copy_1.copy)({
-            resolveFrom: "out",
-            assets: [
-                {
-                    from: "./Source/Configuration/rome.json",
-                    to: "./Configuration/",
-                },
-            ],
-        }),
-    ],
+	format: "esm",
+	minify: true,
+	outdir: Out,
+	platform: "node",
+	target: "esnext",
+	write: true,
+	plugins: [
+		{
+			name: "Target",
+			setup(Build) {
+				Build.onStart(async () => {
+					try {
+						await (0, promises_1.rm)(Out, {
+							recursive: true,
+						});
+					} catch (_Error) {}
+				});
+			},
+		},
+		(0, esbuild_plugin_copy_1.copy)({
+			resolveFrom: "out",
+			assets: [
+				{
+					from: "./Source/Configuration/rome.json",
+					to: "./Configuration/",
+				},
+			],
+		}),
+	],
 };
