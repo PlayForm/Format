0ffd1bd7415394c527c9e3360b3676281b5d4160
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-29 21:34:01 +0300

    squash!

--- Configuration/ESBuild.js
+++ Configuration/ESBuild.js
@@ -1,47 +1,40 @@
 export default {
-	color: true,
-	format: "esm",
-	metafile: true,
-	minify: true,
-	outdir: "Target",
-	platform: "node",
-	target: "esnext",
-	write: true,
-	logLevel: "debug",
-	plugins: [
-		{
-			name: "Target",
-			setup({ onStart, initialOptions: { outdir } }) {
-				onStart(async () => {
-					try {
-						outdir
-							? await (
-									await import("fs/promises")
-							  ).rm(outdir, {
-									recursive: true,
-							  })
-							: {};
-					} catch (_Error) {}
-				});
-			},
-		},
-		(await import("esbuild-plugin-copy")).copy({
-			resolveFrom: "out",
-			assets: [
-				{
-					from: "./Source/Configuration/rome.json",
-					to: "./Configuration/",
-				},
-			],
-		}),
-	],
-	define: {
-		"process.env.VERSION_PACKAGE": `'${
-			(
-				await (
-					await import("typescript-esbuild/Target/Function/JSON.js")
-				).default("package.json")
-			)?.version
-		}'`,
-	},
+    color: true,
+    format: "esm",
+    metafile: true,
+    minify: true,
+    outdir: "Target",
+    platform: "node",
+    target: "esnext",
+    write: true,
+    logLevel: "debug",
+    plugins: [
+        {
+            name: "Target",
+            setup({ onStart, initialOptions: { outdir } }) {
+                onStart(async () => {
+                    try {
+                        outdir
+                            ? await (await import("fs/promises")).rm(outdir, {
+                                recursive: true,
+                            })
+                            : {};
+                    }
+                    catch (_Error) { }
+                });
+            },
+        },
+        (await import("esbuild-plugin-copy")).copy({
+            resolveFrom: "out",
+            assets: [
+                {
+                    from: "./Source/Configuration/rome.json",
+                    to: "./Configuration/",
+                },
+            ],
+        }),
+    ],
+    define: {
+        "process.env.VERSION_PACKAGE": `'${(await (await import("typescript-esbuild/Target/Function/JSON.js")).default("package.json"))?.version}'`,
+    },
 };
