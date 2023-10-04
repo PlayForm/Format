45c1d58a0291d8e04fde491bdb6d0f58251df030
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-05 00:47:20 +0300

    squash!

--- Target/Interface/Rome.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @module Rome
- *
- */
-export default interface Type extends Omit<Configuration, "$schema"> {
-    [key: string]: any;
-}
-import type { Configuration } from "@rometools/wasm-nodejs";
