ca756731eb65e2fc566cb15d0789aa6058567b50
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-31 21:17:41 +0300

    squash!

--- Source/Variable/Option.ts
+++ Source/Variable/Option.ts
@@ -4,7 +4,7 @@
  */
 export default (
 	await import("typescript-esbuild/Target/Function/Merge.js")
-).default((await import("files-pipe/Target/Variable/Option.js")).default, {
+).default((await import("@playform/file-pipe/Target/Variable/Option.js")).default, {
 	Biome: (await import("./Biome.js")).default,
 	Action: {
 		Failed: async (On) => `Error: Cannot format file ${On.Input}!`,
