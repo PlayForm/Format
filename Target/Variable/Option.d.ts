72216a5ea79b78e6b3a7650c25c463e1ebf242fa
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-08 16:52:22 +0300

    squash!
    Signed-off-by: Nikola R. Hristov <nikola@nikolahristov.tech>

--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -2,26 +2,5 @@
  * @module Option
  *
  */
-declare const _default: Omit<{} & {
-    File: string;
-    Biome: any;
-    Cache: {
-        Search: string;
-        Folder: string;
-    };
-    Path: string;
-    Logger: 2;
-    Action: Omit<{} & {
-        Read: ({ Input }: import("@playform/pipe/Target/Interface/File").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("@playform/pipe/Target/Interface/File").default) => Promise<import("@playform/pipe/Target/Type/Buffer").Type>;
-        Passed: (On: import("@playform/pipe/Target/Interface/File").default) => Promise<true>;
-        Failed: (On: any) => Promise<string>;
-        Accomplished: (On: any) => Promise<string>;
-        Fulfilled: ({ File }: {
-            File: any;
-        }) => Promise<string | false>;
-        Changed: (Plan: import("@playform/pipe/Target/Interface/Plan").default) => Promise<import("@playform/pipe/Target/Interface/Plan").default>;
-    }, "__proto__">;
-    Exclude: false;
-}, "__proto__">;
+declare const _default: any;
 export default _default;
