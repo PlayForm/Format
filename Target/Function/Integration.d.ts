477abdfa6e933c16da37b7cf5afab43775671790
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-08 21:06:16 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -6,4 +6,4 @@ declare const _default: Type;
 export default _default;
 import type Type from "@Interface/Integration.js";
 export declare const Default: any;
-export declare const Merge: Type<Generic>;
+export declare const Merge: Interface<Generic>;
