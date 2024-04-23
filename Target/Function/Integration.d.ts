7e48c93dab76407ed4a8802cc1b178f7768704c9
Nikola R. Hristov <nikola@playform.cloud>
2024-04-23 10:38:00 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -5,26 +5,5 @@
 declare const _default: Interface;
 export default _default;
 import type Interface from "@Interface/Integration.js";
-export declare const Default: Omit<{} & {
-    File: string;
-    Biome: any;
-    Cache: {
-        Search: string;
-        Folder: string;
-    };
-    Path: string;
-    Logger: 2;
-    Action: Omit<{} & {
-        Read: ({ Input }: import("@playform/pipe/Target/Interface/File").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("@playform/pipe/Target/Interface/File").default) => Promise<import("@playform/pipe/Target/Type/Buffer").Type>;
-        Passed: (On: import("@playform/pipe/Target/Interface/File").default) => Promise<true>;
-        Failed: (On: any) => Promise<string>;
-        Accomplished: (On: any) => Promise<string>;
-        Fulfilled: ({ File }: {
-            File: any;
-        }) => Promise<string | false>;
-        Changed: (Plan: import("@playform/pipe/Target/Interface/Plan").default) => Promise<import("@playform/pipe/Target/Interface/Plan").default>;
-    }, "__proto__">;
-    Exclude: false;
-}, "__proto__">;
-export declare const Merge: import("@playform/build/Target/Interface/Merge").default<import("@playform/build/Target/Interface/Merge").Generic>;
+export declare const Default: any;
+export declare const Merge: Interface<Generic>;
