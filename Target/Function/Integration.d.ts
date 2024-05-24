498f32348bc54071b94a62847bdad4b3cd8f595f
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-25 01:18:12 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -5,5 +5,22 @@
 declare const _default: Interface;
 export default _default;
 import type Interface from "../Interface/Integration.js";
-export declare const Default: unknown;
-export declare const Merge: import("@playform/build/Target/Interface/Merge.js").default<import("@playform/build/Target/Interface/Merge.js").Generic>;
+export declare const Default: {
+    Biome: any;
+    Action: {
+        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Accomplished: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+    };
+};
+export declare const Merge: <Ts extends readonly unknown[]>(...objects: Ts) => import("deepmerge-ts").DeepMergeHKT<Ts, Readonly<{
+    DeepMergeRecordsURI: "DeepMergeRecordsDefaultURI";
+    DeepMergeArraysURI: "DeepMergeArraysDefaultURI";
+    DeepMergeSetsURI: "DeepMergeSetsDefaultURI";
+    DeepMergeMapsURI: "DeepMergeMapsDefaultURI";
+    DeepMergeOthersURI: "DeepMergeLeafURI";
+    DeepMergeFilterValuesURI: "DeepMergeFilterValuesDefaultURI";
+}>, Readonly<{
+    key: PropertyKey;
+    parents: readonly Readonly<Record<PropertyKey, unknown>>[];
+}>>;
--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -2,5 +2,12 @@
  * @module Option
  *
  */
-declare const _default: unknown;
+declare const _default: {
+    Biome: any;
+    Action: {
+        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Accomplished: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
+        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
+    };
+};
 export default _default;
