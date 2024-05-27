59e470b116875780848f83ab803a7fa95e4fd5af
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-27 21:26:57 +0300

    Format/v0.0.3

--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -3,23 +3,13 @@
  *
  */
 declare const _default: {
-    File: string;
-    Cache: {
-        Search: string;
-        Folder: string;
-    };
-    Path: string;
-    Logger: 2;
+    Biome: any;
     Action: {
-        Failed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
-        Accomplished: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
-        Fulfilled: ({ File }: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
-        Read: ({ Input }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("@playform/pipe/Target/Interface/File.js").default) => Promise<import("@playform/pipe/Target/Type/Buffer.js").Type>;
-        Passed: (On: import("@playform/pipe/Target/Interface/File.js").default) => Promise<true>;
-        Changed: (Plan: import("@playform/pipe/Target/Interface/Plan.js").default) => Promise<import("@playform/pipe/Target/Interface/Plan.js").default>;
+        Failed: (On: any) => Promise<string>;
+        Accomplished: (On: any) => Promise<string>;
+        Fulfilled: ({ File }: {
+            File: any;
+        }) => Promise<string | false>;
     };
-    Exclude: false;
-    Biome: unknown;
 };
 export default _default;
